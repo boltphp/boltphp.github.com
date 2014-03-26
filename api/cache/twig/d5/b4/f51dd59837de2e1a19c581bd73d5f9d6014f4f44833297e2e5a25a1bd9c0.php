27778e0e7833dedc46e182731752033d37b61344
Travis <travis@kuhl.co>
2014-03-25 22:07:20 -0700

    updated API docs

---
+++ api/cache/twig/d5/b4/f51dd59837de2e1a19c581bd73d5f9d6014f4f44833297e2e5a25a1bd9c0.php
@@ -0,0 +1,47 @@
+<?php
+
+/* index.twig */
+class __TwigTemplate_d5b4f51dd59837de2e1a19c581bd73d5f9d6014f4f44833297e2e5a25a1bd9c0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("layout/frame.twig");
+
+        $this->blocks = array(
+            'frame_src' => array($this, 'block_frame_src'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "layout/frame.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_frame_src($context, array $blocks = array())
+    {
+        echo "panel.html";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  28 => 3,);
+    }
+}
